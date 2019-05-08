25c4e51c86ef6220b94c6847110170a5d20255f4
alimranhung <42710065+alimranhung@users.noreply.github.com>
2019-05-08 12:03:23 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+function playKeyboard(event) {
+	const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
+	const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+	if (!audio) return; // stop function from running if no key
+	audio.currentTime = 0; // rewind to start
+	audio.play();
+	key.classList.add('playing');
+}
+
+function removeTransition(event) {
+	if (event.propertyName !== 'transform') return; // skip if its not a transform
+
+	this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+keys.forEach(key => key.addEventListener('click', playMouseClick));
+keys.forEach(key => key.addEventListener('touch', playMouseClick));
+
+window.addEventListener('keydown', playKeyboard);
+
+
+function playMouseClick(event) {
+	const button = event.target.attributes[0].nodeValue;
+
+	console.log(button);
+
+	
+	const audio = document.querySelector(`audio[data-key="${button}"]`)
+	const key = document.querySelector(`.key[data-key="${button}"]`);
+	audio.currentTime = 0; // rewind to start
+	audio.play();
+	key.classList.add('playing');
+}
